1013c6be699dc27126748d6fa477edaab5bae4f9
Shiv Patil <krymzin0311@gmail.com>
2024-05-15 16:57:34 +0530

    feat: theme changer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(inter.className, "dark")}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" disableTransitionOnChange>
+          <Toaster />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,22 +5,26 @@ import MaxWidthDiv from "./layout/MaxWidthDiv";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { SignIn, SignOut } from "./NavAuthButtons";
+import ThemeToggle from "./ui/theme-toggle";
 
 const Navbar = () => {
   const { data: session } = useSession();
   return (
     <nav className="sticky inset-x-0 top-0 z-50 h-16 w-full border-b border-secondary bg-background/75 backdrop-blur-lg transition-all">
       <MaxWidthDiv className="h-full flex-row items-center justify-between border-b">
-        <Link href="/" className="flex gap-4 text-lg font-semibold">
-          <Image
-            src="logo.svg"
-            height={32}
-            width={32}
-            className="h-8 w-8 object-contain"
-            alt="logo"
-          />
-          Marketplace
-        </Link>
+        <div className="flex items-center gap-4 self-stretch">
+          <Link href="/" className="flex gap-4 text-lg font-semibold">
+            <Image
+              src="logo.svg"
+              height={32}
+              width={32}
+              className="h-8 w-8 object-contain"
+              alt="logo"
+            />
+            Marketplace
+          </Link>
+          <ThemeToggle />
+        </div>
 
         <div className="flex h-full items-center gap-4">
           {session ? <SignOut /> : <SignIn />}
